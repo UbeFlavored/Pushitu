c42a636454642b4b585bc85a7b118507ab371123
AldrinAgbanlog <aldrin_john02@yahoo.com>
2014-02-27 00:10:28 +0800

    v0.5
    Feedback System
    Organization View...

--- frontjs/loginCheck.js
+++ frontjs/loginCheck.js
@@ -1,38 +1,40 @@
-function VerifyLogin(){
-	var isEmpty = false;
+function VerifyLogin() {
+    var isEmpty = false;
+    var isEPass = false;
     var i = 0;
     var email = document.getElementById("loginEmail").value;
     var pass = document.getElementById("loginPass").value;
-    console.log("email: "+email);
-    console.log("password: "+pass);
-	
-	for (i = 0; i < document.styleSheets.length; i++) {
-		if (document.styleSheets[i].title == "style")
-			mainCSS = document.styleSheets[i];
-		break;
-	}
-	
-    if(email ==""){
-    	isEmpty = true;
-		document.getElementById("e").className = "form-group has-error";
+    console.log("email: " + email);
+    console.log("password: " + pass);
+
+    for (i = 0; i < document.styleSheets.length; i++) {
+        if (document.styleSheets[i].title == "style")
+            mainCSS = document.styleSheets[i];
+        break;
     }
-	else {
-		document.getElementById("e").className = "form-group";
-		isEmpty = false;
-	}
-    if(pass == ""){
-    	isEmpty = true;
-		document.getElementById("p").className = "form-group has-error";
+
+    if (email == "") {
+        isEmpty = true;
+        document.getElementById("e").className = "form-group has-error";
     }
-	else {
-		document.getElementById("p").className = "form-group";
-		isEmpty = false;
-	}
-    if(isEmpty == true){
-    	return false;
-    }
-    else{
-    	return true;
+    else {
+        document.getElementById("e").className = "form-group";
+        isEmpty = false;
     }
     
+    if (pass == "") {
+        isEPass = true;
+        document.getElementById("p").className = "form-group has-error";
+    }
+    else {
+        document.getElementById("p").className = "form-group";
+        isEPass = false;
+    }
+    if (isEmpty == true || isEPass == true) {
+        return false;
+    }
+    else {
+        return true;
+    }
+
 }
--- frontjs/registerCheck.js
+++ frontjs/registerCheck.js
@@ -1,75 +1,71 @@
-function verifyReg(){
-	var isEmpty = false;
+function verifyReg() {
+    var isEmpty = false;
     var i = 0;
     var forms = document.getElementsByTagName("form")[1].elements;
     var pass = document.getElementById("pass1").value;
-   	var confpass = document.getElementById("pass2").value;
-    // console.log(form[0].value);
-	
-    /*
-   	for(i = 0;i<forms.length;i++){
-   		if(forms[i].value == "" && forms[i].type != "submit"){
-   			isEmpty = true;
-   		}
-   	}
-	*/
-	
-	if(forms[0].value == "" && forms[0].type != "submit"){
-   		document.getElementById("first").className = "form-group has-error";
-		isEmpty = true;
-   	}
-	else {
-		document.getElementById("first").className = "form-group has-success";
-		isEmpty = false;
-	}
-	if(forms[1].value == "" && forms[1].type != "submit"){
-   		document.getElementById("last").className = "form-group has-error";
-		isEmpty = true;
-   	}
-	else {
-		document.getElementById("last").className = "form-group has-success";
-		isEmpty = false;
-	}
-	if(forms[2].value == "" && forms[2].type != "submit"){
-   		document.getElementById("add").className = "form-group has-error";
-		isEmpty = true;
-   	}
-	else {
-		document.getElementById("add").className = "form-group has-success";
-		isEmpty = false;
-	}
-	if(forms[3].value == "" && forms[3].type != "submit"){
-   		document.getElementById("pas1").className = "form-group has-error";
-		isEmpty = true;
-   	}
-	else {
-		document.getElementById("pas1").className = "form-group has-success";
-		isEmpty = false;
-	}
-	if(forms[4].value == "" && forms[4].type != "submit"){
-   		document.getElementById("pas2").className = "form-group has-error";
-		isEmpty = true;
-   	}
-	else {
-		document.getElementById("pas2").className = "form-group has-success";
-		isEmpty = false;
-	}
-	
-   	if(!isEmpty){
-   		if(pass == confpass ){
-			alert("Greetings ");
-			document.getElementById("pas2").className = "form-group has-success";
-   			return true;
-   		}
-   		else{
-   			alert("Password doesn't match");
-			document.getElementById("pas1").className = "form-group has-error";
-			document.getElementById("pas2").className = "form-group has-error";
-			
-   			document.getElementById("pass1").id = "test";
-   			document.getElementById("pass2").id = "test";
-			document.location.reload(true);
-   		}
-   	}
-   	return false;
-   }
+    var confpass = document.getElementById("pass2").value;
+
+    if (forms[0].value == "" && forms[0].type != "submit") {
+        document.getElementById("first").className = "form-group has-error";
+        isEmpty = true;
+    }
+    else {
+        document.getElementById("first").className = "form-group has-success";
+        isEmpty = false;
+    }
+    if (forms[1].value == "" && forms[1].type != "submit") {
+        document.getElementById("last").className = "form-group has-error";
+        isEmpty = true;
+    }
+    else {
+        document.getElementById("last").className = "form-group has-success";
+        isEmpty = false;
+    }
+    if (forms[2].value == "" && forms[2].type != "submit") {
+        document.getElementById("add").className = "form-group has-error";
+        isEmpty = true;
+    }
+    else {
+        document.getElementById("add").className = "form-group has-success";
+        isEmpty = false;
+    }
+    if (forms[3].value == "" && forms[3].type != "submit") {
+        document.getElementById("pas1").className = "form-group has-error";
+        isEmpty = true;
+    }
+    else {
+        document.getElementById("pas1").className = "form-group has-success";
+        isEmpty = false;
+    }
+    if (forms[4].value == "" && forms[4].type != "submit") {
+        document.getElementById("pas2").className = "form-group has-error";
+        isEmpty = true;
+    }
+    else {
+        document.getElementById("pas2").className = "form-group has-success";
+        isEmpty = false;
+    }
+    if (pass != confpass) {
+        document.getElementById("pas1").className = "form-group has-error";
+        document.getElementById("pas2").className = "form-group has-error";
+
+        document.getElementById("pass1").id = "test";
+        document.getElementById("pass2").id = "test";
+        isEmpty = true;
+    }
+
+    if (!isEmpty) {
+        if (pass == confpass) {
+            document.getElementById("pas2").className = "form-group has-success";
+            return true;
+        }
+        else {
+            document.getElementById("pas1").className = "form-group has-error";
+            document.getElementById("pas2").className = "form-group has-error";
+
+            document.getElementById("pass1").id = "test";
+            document.getElementById("pass2").id = "test";
+        }
+    }
+    return false;
+}
